073e6245298887783e2277e289686aee24ce817a
Ma <582011971@qq.com>
2022-08-04 23:26:09 +0800

    push commit1

--- src/main/java/com/ma/git/GitTest.java
+++ src/main/java/com/ma/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("你好 Git3!");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
